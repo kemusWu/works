4ed1e3bf1b65c55d0cbfa687c3bf47642a4101ec
kemusWu <kemus07@gmail.com>
2018-09-04 17:59:23 +0800

    newyear_luck

---
+++ newyear_luck/js/all.js
@@ -0,0 +1,27 @@
+// JavaScript Document
+
+// TOP按鈕滑動效果
+$(function(){
+	
+	$("#gotop").click(function(){
+
+		$("html,body").animate({scrollTop:0},900);
+
+		//$("html,body").animate({scrollTop:0},900,"easeOutBounce");
+
+		return false;
+
+	});
+
+});
+
+// 離開頂端頁才出現TOP按鈕
+ $(".to_top").fadeOut(0);
+    $(window).scroll(function(){
+        if($(window).scrollTop() >= $(".header").offset().top){
+            $('.navbar').css('background','rgba(0,0,0,0.8)');
+            $(".to_top").fadeIn();
+        }else{
+            $(".to_top").fadeOut();
+        }
+    });
